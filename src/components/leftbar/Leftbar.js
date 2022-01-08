d37d3c686aff21fac8adde0f4bb4a248750f44cb
Tushar <tbharti2001@gmail.com>
2022-01-09 02:01:26 +0530

    Some bugs fixed

--- src/components/leftbar/Leftbar.js
+++ src/components/leftbar/Leftbar.js
@@ -5,45 +5,47 @@ import './Leftbar.css'
 export default function Leftbar() {
     return (
         <div className="leftbarContainer">
-            <div className="item">
-                <Home className="icon" />
-                <Typography className="text">Home</Typography>
-            </div>
-            <div className="item">
-                <Person className="icon" />
-                <Typography className="text">Friends</Typography>
-            </div>
-            <div className="item">
-                <ListAlt className="icon" />
-                <Typography className="text">Lists</Typography>
-            </div>
-            <div className="item">
-                <PhotoCamera className="icon" />
-                <Typography className="text">Camera</Typography>
-            </div>
-            <div className="item">
-                <TabletMac className="icon" />
-                <Typography className="text">Apps</Typography>
-            </div>
-            <div className="item">
-                <PlayCircleOutline className="icon" />
-                <Typography className="text">Videos</Typography>
-            </div>
-            <div className="item">
-                <Bookmark className="icon" />
-                <Typography className="text">Collections</Typography>
-            </div>
-            <div className="item">
-                <Storefront className="icon" />
-                <Typography className="text">Market Place</Typography>
-            </div>
-            <div className="item">
-                <Settings className="icon" />
-                <Typography className="text">Settings</Typography>
-            </div>
-            <div className="item">
-                <ExitToApp className="icon" />
-                <Typography className="text">Logout</Typography>
+            <div className="leftbarWrapper">
+                <div className="item">
+                    <Home className="icon" />
+                    <Typography className="text">Home</Typography>
+                </div>
+                <div className="item">
+                    <Person className="icon" />
+                    <Typography className="text">Friends</Typography>
+                </div>
+                <div className="item">
+                    <ListAlt className="icon" />
+                    <Typography className="text">Lists</Typography>
+                </div>
+                <div className="item">
+                    <PhotoCamera className="icon" />
+                    <Typography className="text">Camera</Typography>
+                </div>
+                <div className="item">
+                    <TabletMac className="icon" />
+                    <Typography className="text">Apps</Typography>
+                </div>
+                <div className="item">
+                    <PlayCircleOutline className="icon" />
+                    <Typography className="text">Videos</Typography>
+                </div>
+                <div className="item">
+                    <Bookmark className="icon" />
+                    <Typography className="text">Collections</Typography>
+                </div>
+                <div className="item">
+                    <Storefront className="icon" />
+                    <Typography className="text">Market Place</Typography>
+                </div>
+                <div className="item">
+                    <Settings className="icon" />
+                    <Typography className="text">Settings</Typography>
+                </div>
+                <div className="item">
+                    <ExitToApp className="icon" />
+                    <Typography className="text">Logout</Typography>
+                </div>
             </div>
         </div>
     )
